e22faad4eb5e02f7755e8f08edf2a0f6b8663d23
fernando.camilo <fernando.camilo@fabamaq.com>
2022-05-09 23:54:57 +0100

    Coding Lesson 9 (Video 19) - Input handling

--- source/Private/Window.cpp
+++ source/Private/Window.cpp
@@ -4,6 +4,15 @@
 Window::Window(GLint windowWidth = 800 , GLint windowHeight = 600) {
     m_windowWidth=windowWidth;
     m_windowHeight=windowHeight;
+
+    //set all keys presses to false
+    for (size_t i = 0; i < 1024; i++)
+    {
+        keys[i] = false;
+    }
+
+    deltaX = 0.0f;
+    deltaY = 0.0f;
 }
 
 Window::~Window() {
@@ -43,6 +52,10 @@ bool Window::Init() {
     //Set context for glew (so it know were to draw)
     glfwMakeContextCurrent(m_mainWindow);
     
+    //Mouse and Input Handling
+    CreateCallBacks();
+    glfwSetInputMode(m_mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     //Allow modern extensions
     glewExperimental = GL_TRUE;
     if(glewInit() != GLEW_OK) {
@@ -57,7 +70,16 @@ bool Window::Init() {
     //Setup viewport size
     glViewport(0 , 0, bufferWidth, bufferHeight);
 
+    //Set window pointer ID to the window pointer
+    //and a pointer owner wich is the class that has it
+    glfwSetWindowUserPointer(m_mainWindow, this);
+
     return true;
+}
+
+void Window::CreateCallBacks() {
+    glfwSetKeyCallback(m_mainWindow, HandleKeys);
+    glfwSetCursorPosCallback(m_mainWindow, HandleMouseMovement);
 }
 
 bool Window::GetWindowShouldClose(){
@@ -67,11 +89,58 @@ bool Window::GetWindowShouldClose(){
     return false;
 }
 
-
 void Window::WindowSwapBuffers() {
     //Swap buffer
     if(m_mainWindow)
         glfwSwapBuffers(m_mainWindow);
 }
 
+void Window::HandleKeys(GLFWwindow* window, int key, int code, int action, int mode) {
+    //since its a static function it need a pointer for this class instance
+    Window* currentWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
 
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if(key >= 0 && key < 1024) {
+        if(action == GLFW_PRESS) {
+            currentWindow->keys[key] = true;
+        }
+        else if(action == GLFW_RELEASE) {
+            currentWindow->keys[key] = false;
+        }
+    }
+}
+
+void Window::HandleMouseMovement(GLFWwindow* window, double xPos, double yPos) {
+    //since its a static function it need a pointer for this class instance
+    Window* currentWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    //Set up for the first time
+    if(currentWindow->mouseFirstMoved) {
+        currentWindow->lastX = xPos;
+        currentWindow->lastY = yPos;
+        currentWindow->mouseFirstMoved = false;
+    }
+
+    currentWindow->deltaX = xPos - currentWindow->lastX;
+    currentWindow->deltaY = currentWindow->lastY - yPos;
+
+    currentWindow->lastX = xPos;
+    currentWindow->lastY = yPos;
+}
+
+GLfloat Window::getXChange()
+{
+	GLfloat changed = deltaX;
+	deltaX = 0.0f;
+	return changed;
+}
+
+GLfloat Window::getYChange()
+{
+	GLfloat changed = deltaY;
+	deltaY = 0.0f;
+	return changed;
+}
--- source/Public/Window.h
+++ source/Public/Window.h
@@ -9,6 +9,23 @@ private:
     /* data */
     GLFWwindow* m_mainWindow;
     GLint m_windowWidth, m_windowHeight;
+
+    //Mouse movement
+    GLfloat lastX ,lastY, deltaX, deltaY;
+    bool mouseFirstMoved;
+
+    //Keys pressed
+    bool keys[1024];
+
+    void CreateCallBacks();
+
+    //A callback cant go to a member function
+    //so it needs to be a static function
+    //To be in the context of the window but callable in a callback
+    static void HandleKeys(GLFWwindow* window, int key, int code, int action, int mode); 
+
+    static void HandleMouseMovement(GLFWwindow* window, double xPos, double yPos);
+
 public:
     Window(GLint windowWidth, GLint windowHeight);
 
@@ -16,6 +33,10 @@ public:
     void WindowSwapBuffers();
     bool GetWindowShouldClose();
     
+    bool* getsKeys() { return keys; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+
     ~Window();
 
     inline GLint GetWindowWidth() {return m_windowWidth;};
